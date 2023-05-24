9873807c333ab41526504be24ca6db28372954d3
Ji Xudong <jixudong99@163.com>
2023-05-24 10:13:25 +0800

    添加格式化数字

---
+++ src/components/hooks/useFormater.vue
@@ -0,0 +1,15 @@
+<template>
+  <CommonCard title="今日订单量" :value="numberFormater(orderToday)">
+    <div ref="lineChartRef" style="width: 100%; height: 100%"></div>
+    <template #footer>
+      <div>
+        昨日订单销售量
+        <span class="emphasis">{{ moneyFormater(orderLastDay) }}</span>
+      </div>
+    </template>
+  </CommonCard>
+</template>
+  <script lang="ts" setup>
+import useFormater from "@/hooks/useFormater";
+const { numberFormater, moneyFormater } = useFormater();
+</script>
